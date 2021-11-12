01764c84bedd53bee8eaa8258d47d3dbf7b67bb4
Phil Meyerson <18582693+pmeyerson@users.noreply.github.com>
2021-11-12 14:31:17 -0500

    add describe

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,17 +1,19 @@
-import {render} from '@testing-library/react';
-import { composeStories} from '@storybook/testing-react';
-import * as TaskListStories from './TaskList.stories'; // Our stories under test
+import { render } from '@testing-library/react';
 
-// composeStories will process all the information related to the componenet (e.g. args)
-const { withPinnedTasks} = composeStories(TaskListStories);
+import { composeStories } from '@storybook/testing-react';
 
-describe('TaskList story validation', () => {
-    it('renders pinned tasks at the start of the list', () => {
-        const { container} = render(<TaskListStories.WithPinnedTasks />);
+import * as TaskListStories from './TaskList.stories'; //👈  Our stories imported here
 
-        expect(
-            container.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]')
-        ).not.toBe(null);
-    });
+//👇 composeStories will process all information related to the component (e.g., args)
+const { WithPinnedTasks } = composeStories(TaskListStories);
 
+describe('validate story output', () => {
+
+  it('renders pinned tasks at the start of the list', () => {
+    const { container } = render(<WithPinnedTasks />);
+
+    expect(
+      container.querySelector('.list-item:nth-child(1) input[value="Task 6 (pinned)"]')
+    ).not.toBe(null);
+  });
 });
