e994ef50550777c7ea574995f44ad13dacf63db6
Phil Meyerson <18582693+pmeyerson@users.noreply.github.com>
2021-11-12 14:18:58 -0500

    fix TaskList.propTypes

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -52,13 +52,13 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
             ))}
         </div>
     );
-}
+};
 
 TaskList.propTypes = {
     /** Checks if it's in loading state */
     loading: PropTypes.bool,
     /** The list of tasks */
-    tasks: PropTypes.arrayof(Task.PropTypes.task).isRequired,
+    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
     /** Event to change the task to pinned */
     onPinTask: PropTypes.func,
     /** Event to change the task to archived */
